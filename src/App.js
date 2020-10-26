93a2470edf7d2d527a6ef11e5b89db6ee6ab8b67
KF Gomez <fgomez@mangochango.com>
2020-10-25 20:12:56 -0600

    init project sample

--- src/App.js
+++ src/App.js
@@ -1,23 +1,122 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { create } from "./dfa";
 
 function App() {
+  const [stateName, setStateName] = useState("");
+  const [transitionValue, setTransitionValue] = useState("");
+  const [transition, setTransition] = useState("");
+
+  const [transitions, setTransitions] = useState([]);
+  const [states, setStates] = useState([]);
+
+  const [definition, setDefinition] = useState({});
+  const [testInput, setTestInput] = useState("");
+
+  const handleAddState = () => {
+    let transitionsFormatted = {};
+    transitions.forEach((tr) => {
+      transitionsFormatted = { ...transitionsFormatted, ...tr };
+    });
+    const newState = {
+      name: stateName,
+      transitions: transitionsFormatted,
+    };
+
+    setStates([...states, newState]);
+    setTransition("");
+    setStateName("");
+    setTransitionValue("");
+    setTransitions([]);
+  };
+
+  const handleAddTransition = () => {
+    const newTransition = { [transition]: transitionValue };
+    const newTransitions = [...transitions, newTransition];
+    setTransitions(newTransitions);
+  };
+
+  const handleCreateDefinition = () => {
+    let formattedStates = {};
+    states.forEach((st) => {
+      formattedStates = { ...formattedStates, [st.name]: st.transitions };
+    });
+    const newDefinition = {
+      start: "START",
+      finals: ["SB"],
+      states: formattedStates,
+    };
+    setDefinition(newDefinition);
+  };
+
+  const validateTestInput = () => {
+    const machine = create(definition);
+    console.log(machine.accept(testInput));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        Current:
+        <div>
+          {states.map((s, i) => {
+            return (
+              <div key={i}>
+                <h4>{s.name}</h4>
+                <ul>{JSON.stringify(s.transitions)}</ul>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <div>
+        <h3>Define states</h3>
+        <div>
+          name:{" "}
+          <input
+            value={stateName}
+            onChange={(e) => setStateName(e.target.value)}
+          />
+          <br />
+          add transition:{" "}
+          <input
+            onChange={(e) => setTransition(e.target.value)}
+            value={transition}
+          />{" "}
+          {"->"}{" "}
+          <input
+            onChange={(e) => setTransitionValue(e.target.value)}
+            value={transitionValue}
+          />{" "}
+          <button onClick={handleAddTransition}>Add transition</button>
+          <div>
+            transitions:
+            <ul>
+              {transitions.map((tr, idx) => (
+                <li key={idx}>{JSON.stringify(tr)}</li>
+              ))}
+            </ul>
+          </div>
+          <div>
+            <button onClick={handleAddState}>save state</button>
+          </div>
+          <div>
+            <button onClick={handleCreateDefinition}>
+              generate definition
+            </button>
+          </div>
+        </div>
+      </div>
+
+      {Object.keys(definition).length !== 0 && (
+        <div>
+          validate string
+          <input
+            value={testInput}
+            onChange={(e) => setTestInput(e.target.value)}
+          />
+          <button onClick={validateTestInput}>validate</button>
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/dfa.js
@@ -0,0 +1,141 @@
+function validate(definition) {
+  var error;
+
+  function forEachFinal(f) {
+    var i, result;
+
+    for (i = 0; i < definition.finals.length; i += 1) {
+      result = f(definition.finals[i]);
+
+      if (result) {
+        return result;
+      }
+    }
+  }
+
+  function forEachTransition(f) {
+    var state, character, destination, result;
+
+    for (state in definition.states) {
+      if (definition.states.hasOwnProperty(state)) {
+        for (character in definition.states[state]) {
+          if (definition.states[state].hasOwnProperty(character)) {
+            destination = definition.states[state][character];
+
+            result = f(state, character, destination);
+
+            if (result) {
+              return result;
+            }
+          }
+        }
+      }
+    }
+  }
+
+  function isStateUndefined(name) {
+    return definition.states.hasOwnProperty(name) === false;
+  }
+
+  // Definition is mandatory
+
+  if (!definition) {
+    return "Invalid definition";
+  }
+
+  // Start state is mandatory
+
+  if (!definition.start) {
+    return "Start state not specified";
+  }
+
+  // Final states are mandatory
+
+  if (!definition.finals) {
+    return "Final states not specified";
+  }
+
+  // At least one final state must be specified
+
+  if (definition.finals.length === 0) {
+    return "No final states listed";
+  }
+
+  // Definition of states must be specified
+
+  if (!definition.states) {
+    return "No states specified";
+  }
+
+  // Definition of start state must be specified
+
+  if (isStateUndefined(definition.start)) {
+    return "The initial state has no definition";
+  }
+
+  // Definition of every final state must be specified
+
+  error = forEachFinal(function (f) {
+    if (isStateUndefined(f)) {
+      return "Final state '" + f + "' has no definition";
+    }
+  });
+
+  if (error) {
+    return error;
+  }
+
+  // Each state must process one character at a time and the destination must have a
+  // definition
+
+  error = forEachTransition(function (state, character, destination) {
+    if (character.length !== 1) {
+      return "Invalid character '" + character + "' for state '" + state + "'";
+    }
+
+    if (isStateUndefined(destination)) {
+      return (
+        "State '" +
+        state +
+        "' has a non-existing destination '" +
+        destination +
+        "'"
+      );
+    }
+  });
+
+  if (error) {
+    return error;
+  }
+}
+
+export const create = (definition) => {
+  var error;
+
+  error = validate(definition);
+
+  if (error) {
+    throw new Error(error);
+  }
+
+  return {
+    accept: function (s) {
+      var current, i;
+
+      if (typeof s !== "string") {
+        throw new Error("Input is not a string");
+      }
+
+      current = definition.start;
+
+      // During the process, current can be undefined because of unrecognized character
+      // for the current state. We can stop the loop as soon as current becomes undefined.
+
+      for (i = 0; i < s.length && current; i += 1) {
+        current = definition.states[current][s[i]];
+      }
+
+      return definition.finals.indexOf(current) !== -1;
+    },
+  };
+};
